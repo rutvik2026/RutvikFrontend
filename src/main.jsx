e7f6882660554f350950f696e9f62ff91f706b19
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-01-22 23:02:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { AuthContext } from "./context/AuthContext"; // Import your context
-import "./index.css";
 
-// Define the initial context value
-const authContextValue = {
-  role: "user", // You can dynamically manage this value later
-  isLoggedIn: true, // Adjust based on your app logic
-};
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById("root")).render(
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* Wrap App with AuthContext.Provider */}
-    <AuthContext.Provider value={authContextValue}>
-      <App />
-    </AuthContext.Provider>
-  </StrictMode>
-);
+  
+    <App />
+  </StrictMode>,
+)
