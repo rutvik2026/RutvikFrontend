164a0187a2a6c254a91ef2bee58753ae1ee15358
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-01-23 09:55:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://backend-8-tuix.onrender.com", 
+        target: "https://backend-8-tuix.onrender.com",
         changeOrigin: true,
       },
+    },
+    historyApiFallback: true, // Ensures SPA fallback for dev server
+  },
+  build: {
+    rollupOptions: {
+      input: "./index.html", // Ensure this is the correct entry point
     },
   },
   plugins: [react()],
