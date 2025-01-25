6f917266a81fecb113c41f38c586ca5f776346ef
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-01-25 20:13:43 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import { Container } from "react-bootstrap";
 
 export const Home = () => {
   const [data, setData] = useState([]);
-
+  const [query, setQuery] = useState('');
   useEffect(() => {
     
     const fetchData = async () => {
       try {
         const baseUrl = import.meta.env.VITE_API_BASE_URL;
-        const response = await axios.get(`${baseUrl}/owners`);
+        const response = await axios.get(`${baseUrl}/owners?q=${query}`);
         const result = await response.data;
         setData(result);
         console.log("fetched data",result);
@@ -22,10 +22,16 @@ export const Home = () => {
     };
 
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
     <div className="d-flex flex-wrap mt-3 justify-content-center">
+        <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search menu items"
+      />
       <Container className="mt-3">
         <img
           src="https://cdn.thespicery.com/media/user_uploads/cache/2560x850/Mezze-Hero-2560x850px_1_1_.jpg"
