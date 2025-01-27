6b1a6d3fde837ebee06c6035d8d3f89d8bfb13b1
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-01-27 08:53:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -120,7 +120,7 @@ function App() {
             <Route path="/login" element={<Login onLogin={handleLogin} />} />
           <Route
             path="/home"
-            element={<Protected isLoggedIn={isLoggedIn}><Home /></Protected>}
+            element={<Home />}
           />
           <Route
             path="/ownerhome"
